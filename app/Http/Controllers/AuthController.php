0452dff417e7e39ec25344718b44a1af261a3e05
hesaa <hesa7511@gmail.com>
2022-07-07 01:16:29 +0700

    Login / Register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $_data = $request->only('email', 'password');
+
+        $validate = Validator::make($_data, [
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8',
+        ])->errors()->all();
+
+        if ($validate) {
+            return $this->retrunScema($_data, 400, $validate);
+        }
+
+
+        $token = Auth::attempt($_data);
+
+        if (!$token) {
+            $status = 401;
+            return $this->retrunScema($_data, $status, ['Email / Pasword invalid.']);
+        }
+
+        $user = Auth::user();
+
+        $login = [
+            'user' => $user,
+            'auth' => [
+                'token' => $token,
+                'type' => 'Bearer'
+            ]
+        ];
+        return $this->retrunScema($login, 200);
+    }
+
+    public function register(Request $request)
+    {
+        $_data = $request->only('name', 'email', 'password');
+
+        $validate = Validator::make($_data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ])->errors()->all();
+
+        if ($validate) {
+            return $this->retrunScema($_data, 400, $validate);
+        }
+
+        $store = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = Auth::login($store);
+        $register = [
+            'user' => $store,
+            'auth' => [
+                'token' => $token,
+                'type' => 'Bearer',
+            ]
+        ];
+        return $this->retrunScema($register, 201);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return $this->retrunScema([
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function refresh()
+    {
+        return $this->retrunScema([
+            'user' => Auth::user(),
+            'auth' => [
+                'token' => Auth::refresh(),
+                'type' => 'Bearer',
+            ]
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function retrunScema($data, $status = 200, $error = [])
+    {
+        $return = [
+            'status' => $status,
+            'data' => $data,
+        ];
+
+        if (!in_array($status, [
+            200, 201, 202, 203, 204, 205, 206,
+        ])) {
+            if (empty($data)) {
+                unset($return['data']);
+            }
+            $return['errors'] = $error;
+        }
+        return (object) response()->json($return, $status);
+    }
+}
